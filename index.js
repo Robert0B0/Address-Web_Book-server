7bdeacdb9ab2a00aa5eaa873a51b8479dd8e9b20
Robert0B0 <70260478+Robert0B0@users.noreply.github.com>
2022-01-12 15:09:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+	{origin: 'https://web-contacts-app.herokuapp.com'}
+));
 app.listen(port, () => {
 	console.log("Server started on port", port);
 });

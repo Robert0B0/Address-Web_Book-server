dac7b418cc5ca83683fe81be999b9f098333a0a5
Robert0B0 <70260478+Robert0B0@users.noreply.github.com>
2022-01-13 17:13:43 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,20 @@
+const Pool = require("pg").Pool;
+
+const poolConfig = process.env.DATABASE_URL
+	? {
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnAuthorized: false,
+			},
+	  }
+	: {
+			user: "postgres",
+			password: "MyPa$$4Dew",
+			database: "test_db",
+			host: "localhost",
+			port: 5433,
+	  };
+
+const pool = new Pool(poolConfig);
+
+module.exports = pool;
